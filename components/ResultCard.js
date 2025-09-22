2e693fb2182bb781cd02a997d69d2ade7f48acff
riteshshas <riteshshastri2006@gmail.com>
2025-09-22 22:00:08 +0530

    Update ResultCard.js

--- components/ResultCard.js
+++ components/ResultCard.js
@@ -6,7 +6,8 @@ export default function ResultCard({ result }) {
       border: "1px solid #ddd",
       borderRadius: "12px",
       padding: "20px",
-      marginTop: "20px"
+      marginTop: "20px",
+      whiteSpace: "pre-wrap"
     }}>
       <h3>Result</h3>
       <pre>{result}</pre>
